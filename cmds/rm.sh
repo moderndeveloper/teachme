5dd27dcadc62ef8ed6d751b40aa462a9c8ff69da
Jing Ran <jingran.mavis@gmail.com>
2016-02-29 23:18:46 -0500

    Add rm syntax

---
+++ cmds/rm.sh
@@ -0,0 +1,28 @@
+echo -e 'test'
+
+# the command's description
+echo "description here"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "rm [-fiRrv] file ..."
+echo -e "\t-f: Force remove files without prompting for confirmation."
+echo -e "\t-i: Prompt confirmation before every removal"
+echo -e "\t-R or -r: Recursive; delete non-empty directories and their contents"
+echo -e "\t-v: Print files as they are removed"
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
