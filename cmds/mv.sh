7a265711ba273c5b9a50387d5a0b77e8b2989b53
Jing Ran <jingran.mavis@gmail.com>
2016-02-29 23:16:55 -0500

    Add mv description

--- cmds/mv.sh
+++ cmds/mv.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
-echo "This is a test command."
+echo "This is a mv command."
 
 # The command's name
-echo -e 'test'
+echo -e 'mv'
 
 # the command's description
-echo "description here"
+echo "The mv command moves or renames files. If two files are given as the command arguments, mv renames the first as the second. If the first argument is a file and the second is an existing directory, the command moves the given file (first argument) to the existing directory."
 
 echo "------------"
 
