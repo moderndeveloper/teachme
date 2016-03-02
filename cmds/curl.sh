1bec3afbe1a7a928ea2629f9a158a70eed91c152
Jing Ran <jingran.mavis@gmail.com>
2016-03-01 23:02:05 -0500

    Create curl branch and add description

---
+++ cmds/curl.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a curl command."
+
+# The command's name
+echo -e 'curl'
+
+# the command's description
+echo "The curl command transfers data from or to a server, using of the protocols: HTTP, HTTPS, FTP, FTPS, SCP, SFTP, TFTP, DICT, TELNET, LDAP OR FILE. The results will be displayed on terminal."
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
