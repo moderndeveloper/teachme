b396d41a45e57eda4492850f71505abf90dfa653
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 15:52:16 -0800

    Add git merge description

---
+++ cmds/git/merge.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a git merge command."
+
+# The command's name
+echo -e 'git merge'
+
+# the command's description
+echo "It joins two or more development histories together by incorporating changes since their histories were split from the current branch, into the current branch."
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "git test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "git test"
+echo description here
+
+# example 2
+echo "git test something"
+echo description here
+
+
+exit 0
