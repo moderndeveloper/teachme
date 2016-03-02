d358b5c75eac2bbed1b7e7ddf9a18f8c19480c2a
Jing Ran <jingran.mavis@gmail.com>
2016-03-01 23:04:21 -0500

    Create pipe branch and add syntax

---
+++ cmds/pipe.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+echo "This is a test command."
+
+# The command's name
+echo -e 'test'
+
+# the command's description
+echo "description here"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "grep James names.txt | sort"
+echo 'First, the grep command finds lines with "James" in the file names.txt. Then, pipe sends the output of the grep command as the input of the sort command.'
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
