ec9c2954179134d9a82032cb7d59ad7c7ced4406
Jing Ran <jingran.mavis@gmail.com>
2016-03-01 23:07:59 -0500

    Create status branch and add description

---
+++ cmds/git/status.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a git status command."
+
+# The command's name
+echo -e 'git status'
+
+# the command's description
+echo "The git status command displays the status of the working directory. It shows which changes have been added to staging area, which have not, and which are not being tracked by Git."
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "git test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "git test"
+echo description here
+
+# example 2
+echo "git test something"
+echo description here
+
+
+exit 0
