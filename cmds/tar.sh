6240103f74f4aa01e0383e9cc2296d3d895f99e5
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 13:05:43 -0800

    Update tar description

--- cmds/tar.sh
+++ cmds/tar.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-echo "This is a test command."
+echo "This is a tape archive command-line command."
 
 # The command's name
-echo -e 'test'
+echo -e 'tar'
 
 # the command's description
-echo "description here"
-
+echo "It stands for tape archive which is used to highly compress archive files in .tar, .tar.gz (also called gzip) or .tar.gz2 (also called bzip2) formats."
+echo "The difference between these formats are that .tar can compress multiples files into a single tar file, while gzip and bzip2 only compress single files."
 echo "------------"
 
 # the command's syntax
