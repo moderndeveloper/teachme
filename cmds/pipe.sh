bd0984fea8efc715f94413e9f6fa967f4c53c218
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 14:08:10 -0800

    Add pipe examples

--- cmds/pipe.sh
+++ cmds/pipe.sh
@@ -19,12 +19,12 @@ echo "------------"
 
 # the command's usage examples
 # example 1
-echo "test"
-echo description here
+echo "ls | wc -l > myfile.txt"
+echo "List all the files (ls) in present working directory then take this output and pass it into 'wc' command to count the number of lines (-l) then print the result to a file called 'myfile.txt'"
 
 # example 2
-echo "test something"
-echo description here
+echo "cat * | grep "melon" | grep -v "cantaloupe" | wc -l"
+echo "concatenate all files (cat *) in present working directory and search for 'melon'. Take the output of files containing 'melon' and do another search that is not (-v) 'cantaloupe' then do a line count (wc -l)"
 
 
 exit 0
