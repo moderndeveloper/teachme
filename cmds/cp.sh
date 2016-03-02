6630bc375b8611b823877d84dbdc99390191908d
Jing Ran <jingran.mavis@gmail.com>
2016-03-01 22:57:11 -0500

    Create cp branch and add description

---
+++ cmds/cp.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a cp command."
+
+# The command's name
+echo -e 'cp'
+
+# the command's description
+echo "The cp command copies the contents of a file to another file. Or it copies multiple files to a directory. Copying a file to itself will fail."
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
