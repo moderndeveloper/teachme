c807eb5b3ed0eab8e3617b63f636cd016a3382d3
Jing Ran <jingran.mavis@gmail.com>
2016-03-01 22:44:42 -0500

    Add syntax for find

--- cmds/find.sh
+++ cmds/find.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-echo "This is a git test command."
+echo "This is a git command."
 
 # The command's name
-echo -e 'git test'
+echo -e 'git'
 
 # the command's description
 echo "description here"
@@ -11,9 +11,10 @@ echo "------------"
 
 # the command's syntax
 echo "Syntax:"
-echo "git test argument1 argument2"
-echo -e "\targument1: description of argument 1"
-echo -e "\targument2: description of argument 2"
+echo 'find ~ -type f -name "section*" -exec rm {} \;'
+echo -e "\t-type: Type to find; f means regular file; d means directory"
+echo -e "\t-name: Find files or directories with the given name"
+echo -e "\t-exec: Execute command; the results before -exec option are taken to be arguments to the command after the option and replace the string {}; the expression must be terminated by a semicolon (;) and may need to be escaped."
 #   ...      etc
 
 echo "------------"
