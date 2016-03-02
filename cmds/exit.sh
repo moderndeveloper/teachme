628764fb7e42f68f159c6ff63d14f223e7e9bf25
Jing Ran <jingran.mavis@gmail.com>
2016-03-01 22:52:39 -0500

    Create exit branch and add syntax

---
+++ cmds/exit.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+echo "This is a test command."
+
+# The command's name
+echo -e 'test'
+
+# the command's description
+echo "description here"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "exit"
+echo "No flags are needed."
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
