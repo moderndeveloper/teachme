202c13988db9d7f2afbecd1c69582a9c550f29ab
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 15:14:21 -0800

    Add description for git add

---
+++ cmds/git/add.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a git add command."
+
+# The command's name
+echo -e 'git add'
+
+# the command's description
+echo "Add contents of the specified file at the time command is run to a staging area waiting to be committed with git commit"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "git test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "git test"
+echo description here
+
+# example 2
+echo "git test something"
+echo description here
+
+
+exit 0
