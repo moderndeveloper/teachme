50948ca12de75639f34cc84175dfadc4ed9ce9e2
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-01 18:26:01 -0800

    Add man syntax

---
+++ cmds/git/man.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+echo "This is a test command."
+
+# The command's name
+echo -e 'test'
+
+# the command's description
+echo "description here"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax: man [options] keyword"
+echo "man pwd"
+echo "Display manual for 'pwd' command"
+
+echo "man -af 1 ls"
+echo "Display section 1, User Commands, of manual pages for all that matches 'ls' command in one-liner format for each file"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
