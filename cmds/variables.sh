46b6ff7d604ebf41645df33eae09dcffbffb5a87
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 18:17:54 -0800

    Add variables syntax

---
+++ cmds/variables.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a test command."
+
+# The command's name
+echo -e 'test'
+
+# the command's description
+echo "description here"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax: variable=value"
+echo -e "myvar='This is a variable'"
+echo -e "$myvar \nThis will output the stored value of 'myvar'"
+
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
