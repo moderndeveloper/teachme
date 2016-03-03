8d68b05fb8166eb62eee248d236cd8d1d5f8ce88
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 17:23:17 -0800

    Add description for chmod

---
+++ cmds/chmod.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a command-line chmod command."
+
+# The command's name
+echo -e 'chmod'
+
+# the command's description
+echo "It is used to change permissions of files or directories"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "test"
+echo description here
+
+# example 2
+echo "test something"
+echo description here
+
+
+exit 0
