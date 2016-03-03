57b422126908286e6866d7deef095d4402822328
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 17:05:06 -0800

    Add rebase description

---
+++ cmds/git/rebase.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a git rebase command."
+
+# The command's name
+echo -e 'git rebase'
+
+# the command's description
+echo "Merge a branch to the current working branch and move all the local commits that are ahead of the rebased branch to the top of the commit history"
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "git test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "git test"
+echo description here
+
+# example 2
+echo "git test something"
+echo description here
+
+
+exit 0
