e1a4d251b38aedadc6d9a640a564ea8391a4f8fd
Jennifer Chiang <jchiang7874@yahoo.com>
2016-03-02 16:22:09 -0800

    Add git log description

---
+++ cmds/git/log.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "This is a git log command."
+
+# The command's name
+echo -e 'git log'
+
+# the command's description
+echo "It lists the commits made in the working directory beginning with the most recent commit."
+
+echo "------------"
+
+# the command's syntax
+echo "Syntax:"
+echo "git test argument1 argument2"
+echo -e "\targument1: description of argument 1"
+echo -e "\targument2: description of argument 2"
+#   ...      etc
+
+echo "------------"
+
+# the command's usage examples
+# example 1
+echo "git test"
+echo description here
+
+# example 2
+echo "git test something"
+echo description here
+
+
+exit 0
