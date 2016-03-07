ae6230e4a092e4304a472e08da83bd1d0f91b9ef
irom77 <irek.romaniuk@gmail.com>
2016-03-07 12:08:42 -0500

    develop-git config description added

--- cmds/git/config.sh
+++ cmds/git/config.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
-echo "This is a git test command."
+echo "This is a git config command."
 
 # The command's name
-echo -e 'git test'
+echo -e 'git config'
 
 # the command's description
-echo "description here"
+echo "git-config - Get and set repository or global options"
 
 echo "------------"
 
